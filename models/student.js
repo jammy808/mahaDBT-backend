7bbf9d55d9cdc5859ff6371ec43a0d49a27a2295
Soham <jambhwadekarsoham@gmail.com>
2024-08-29 02:40:32 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -3,14 +3,6 @@ const plm = require("passport-local-mongoose");
 
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((error) => {
-//     console.error('Could not connect to MongoDB:', error.message);
-//   });
-
 const studentSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -19,12 +11,10 @@ const studentSchema = mongoose.Schema({
   email: String,
   token: Number,
   verified: Boolean,
-  // events: [
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "Event",
-  //   },
-  // ],
+  birthDate: Date,
+  mobileNo: String,
+  address: String,
+  scholarship: { type: mongoose.Schema.Types.ObjectId, ref: 'Scholarship' }
 });
 
 studentSchema.plugin(plm);
